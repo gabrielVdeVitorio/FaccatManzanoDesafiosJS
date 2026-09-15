8a9e9306d2c9a56eb43d396228bcd2fa0b63cee9
Mateus-Isaque <mateusisaque2007@gmail.com>
2026-09-15 14:49:48 -0300

    Added click event listeners to hide the actual exercise and displays the next. Missing: import functions to unify the execution in just one file.

--- js/button.js
+++ js/button.js
@@ -1,20 +1,45 @@
-let previousExercise, actual;
+let previousExercise, disableButtonFunction;
+let actual = 1;
+
+const previousButton = document.getElementById('previousButton');
+const nextButton = document.getElementById('nextButton');
+
 const exercises =
 {
-    '1': document.getElementById('firstExercise')
+    '1': document.getElementById('firstExercise'),
+    '2': document.getElementById('secondExercise'),
+    '3': document.getElementById('thirdExercise')
 }
 
-const previousButton = () =>
+const disableButton =
 {
-
+    '1': () => { previousButton.setAttribute.disabled = true },
+    '3': () => { nextButton.setAttribute.disabled = true }
 }
 
-const restartButton = () =>
+const main = () =>
 {
+    previousButton.addEventListener( 'click', () =>
+    {
+        // Disable the button previous button if it's the first exercise.
+        disableButtonFunction = disableButton[actual];
+        disableButtonFunction();
+        
+        // Hide actual, point actual to the previous position and display it.
+        exercises[actual].style.display = 'none';
+        actual -= 1;
+        exercises[actual].style.display = 'flex';
+    });
 
-}
-
-const nextButton = () =>
-{
-
+    nextButton.addEventListener( 'click', () =>
+    {
+        // Disable the button previous button if it's the first exercise.
+        disableButtonFunction = disableButton[actual];
+        disableButtonFunction();
+        
+        // Hide actual, point actual to the next position and display it.
+        exercises[actual].style.display = 'none';
+        actual += 1;
+        exercises[actual].style.display = 'flex';
+    });
 }
