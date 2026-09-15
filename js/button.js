f16478e6856006668a8b3cc0c5f3edcb1e6f3033
bendominique <benbenjamim936@gmail.com>
2026-09-15 09:00:06 -0300

    Started to unify contents; firstExercise in progress, fixing styles.

---
+++ js/button.js
@@ -0,0 +1,20 @@
+let previousExercise, actual;
+const exercises =
+{
+    '1': document.getElementById('firstExercise')
+}
+
+const previousButton = () =>
+{
+
+}
+
+const restartButton = () =>
+{
+
+}
+
+const nextButton = () =>
+{
+
+}
